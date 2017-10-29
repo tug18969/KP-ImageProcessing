989411f741603f902853068a6ffce81db83f4b6e
tug18969 <tug18969@temple.edu>
2017-10-29 16:05:35 -0400

    Added TestClassifier module
    TestClassifier is meant to be used in a python workspace. Contains one method, predict, for calling the underlying keras model's predict method.

---
+++ Neural Network Classification/TestClassifier.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import tensorflow as tf
+import cv2
+import keras
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np    
+import os
+
+class FoodClassifier:
+    
+    def __init__(self, modelpath, labels):
+        self.model = load_model(modelpath)
+        self.labels = labels
+    
+    def predict(self,img):
+        
+        #check if image is a filepath
+        if(isinstance(img,str)):
+            if(not os.path.exists(img)):
+                print("Error: Invalid File Path")
+                return ""
+            else:
+                #if its a filepath, convert to PIL image
+                img = Image.open(img)
+        
+        #resize image
+        imgr = img.resize((128,128))
+        x = img_to_array(imgr).reshape((1,128,128,3))
+        
+        #predict
+        prediction = self.model.predict(x)
+        #print(prediction.shape)
+        #print(prediction)
+        
+        #get max of predictions and return labels
+        predIdx = np.argmax(prediction[0,:])
+        return self.labels[predIdx]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
